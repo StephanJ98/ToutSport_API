dad013fbd1981a7557b0dd7d72adad5b552eeb40
Diégo Stéphan Jeandon Rodríguez <diego.jeandon@gmail.com>
2021-02-21 15:39:55 +0100

    :rocket: Listing methods

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const cors = require('cors');
 const url = require('url');
 var port = process.env.PORT || 4000;
 
+let data = require('./data')
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.send(JSON.stringify("OK"));
+    res.sendStatus(200)
+});
+
+app.get("/list", (req, res) => {
+    res.json(data);
+});
+
+app.get("/list/:id", (req, res) => {
+    const elemId = req.params.id
+    if (Object.keys(data).length - 1 >= elemId) {
+        res.json(data[elemId])
+    } else {
+        res.sendStatus(404)
+    }
 });
 
 app.listen(port, function () {
-    console.log(`API listening!`);
+    console.log(`API running on port: ${port}`);
 });
